90e17ac684270cc59b62a60e9e87fd765c9b5c43
Anurag <anuragrajanp@gmail.com>
2024-03-07 22:20:03 +0530

    models(edit): attendance and timetable schema

--- models/Attendance.js
+++ models/Attendance.js
@@ -5,6 +5,30 @@ const AttendanceSchema = new mongoose.Schema({
 		type: mongoose.Schema.Types.ObjectId,
 		ref: "Student",
 	},
+	semesterId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Semester",
+	},
+	courseId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Course",
+	},
+	date: {
+		type: Date,
+		required: true,
+	},
+	hour: {
+		type: Number,
+		required: true,
+	},
+	present: {
+		type: Boolean,
+		default: false,
+	},
+	facultyId: {
+		type: type: mongoose.Schema.Types.ObjectId,
+		ref: "Faculty",
+	}
 });
 
 const Attendance = mongoose.model("Attendance", AttendanceSchema);
--- models/TimeTable.js
+++ models/TimeTable.js
@@ -1,10 +1,30 @@
 import mongoose from "mongoose";
 
 const TimeTableSchema = new mongoose.Schema({
+	studentId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Student",
+	},
+	semesterId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Semester",
+	},
 	courseId: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: "Course",
 	},
+	day: {
+		type: String,
+		required: true,
+	},
+	hour: {
+		type: Number,
+		required: true,
+	},
+	facultyId: {
+		type: type: mongoose.Schema.Types.ObjectId,
+		ref: "Faculty",
+	}
 });
 
 const TimeTable = mongoose.model("TimeTable", TimeTableSchema);
