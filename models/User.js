3a5c8479ad3a35c37ab8bbb1d92d35781db2f45d
Abdul Majid <majid616365@gmail.com>
2024-03-21 20:49:52 +0530

    user routes setup

--- models/User.js
+++ models/User.js
@@ -17,6 +17,7 @@ const UserSchema = new mongoose.Schema(
     },
     password: {
       type: String,
+      select: false,
       required: true,
     },
     type: {
--- routes/User.js
+++ routes/User.js
@@ -1,38 +1,54 @@
 import express from "express";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import joi from "joi";
 
 const router = express.Router();
+const loginSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().min(8).required(),
+});
+
+const signupSchema = joi.object({
+  name: joi.string().required(),
+  gender: joi.string().valid("M", "F").required(),
+  email: joi.string().email().required(),
+  password: joi.string().min(8).required(),
+  type: joi.string().valid("student", "faculty", "admin", "parent").required(),
+});
 
 router.post("/register", async (req, res) => {
   try {
-    const { name, gender, email, password, type } = req.body;
+    const { value: data, error } = signupSchema.validate(req.body);
 
-    if (!name || !gender || !email || !password || !type) {
-      return res.status(400).send({ message: "All fields are required" });
+    if (error) {
+      return res
+        .status(400)
+        .json({ message: error.details[0].message, success: false });
     }
 
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email: data.email });
 
     if (existingUser) {
       return res.status(400).send({ message: "User already exists" });
     }
 
-    const hashedPassowrd = await bcrypt.hash(password, 10);
+    const hashedPassowrd = await bcrypt.hash(data.password, 10);
 
     const user = new User({
-      name,
-      gender,
-      email,
-      type,
+      name: data.name,
+      email: data.email,
+      gender: data.gender,
+      type: data.type,
       password: hashedPassowrd,
     });
 
     await user.save();
 
-    res.send({ message: "User registered successfully", data: user });
+    res.send({ message: "User registered successfully", data: user, success: true});
   } catch (error) {
-    res.status(500).send({ message: error.message });
+    res.status(500).send({ message: error.message , success: false});
   }
 });
 
@@ -65,6 +81,50 @@ router.patch("/updatepassword", async (req, res) => {
     await user.save();
 
     res.json({ message: "Password updated successfully", success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", success: false });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { value: data, error } = loginSchema.validate(req.body);
+
+  if (error) {
+    return res
+      .status(400)
+      .json({ message: error.details[0].message, success: false });
+  }
+
+  try {
+    const user = await userModel
+      .findOne({ email: data.email })
+      .select("+password");
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Invalid email or password", success: false });
+    }
+
+    const isPasswordValid = await bcrypt.compare(data.password, user.password);
+
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ message: "Invalid email or password", success: false });
+    }
+
+    const payload = {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.json({ message: "Logged in successfully", token, success: true });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error", success: false });
   }
