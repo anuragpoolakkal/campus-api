8bc3d27b7e2fa2581f34ef942893fbce4183030b
Rithas2003 <rithasumy@gmail.com>
2024-03-26 18:40:31 +0530

    joi validation added for scheme

--- routes/scheme.js
+++ routes/scheme.js
@@ -1,39 +1,17 @@
 import express from "express";
-import Scheme from "../models/Scheme.js";
-import joi from "joi";
+import SchemeModel from "../models/Scheme.js";
+import CourseModel from "../models/Course.js";
 
+import joi from "joi";
+import mongoose from "mongoose";
 const router = express.Router();
 
-const schemeSchema = joi.object({
-    totalMarks: joi.number().required(),
-    parameters: joi.array()
-        .items(
-            joi.object({
-                name: joi.string().required(),
-                weightage: joi.number().required().min(0).max(100),
-            }),
-        )
-        .required(),
-});
 
 // Get all schemes
-router.get("/scheme", async (req, res) => {
+router.get("/", async (req, res) => {
     try {
-        const schemes = await Scheme.find();
-
-        if (schemes.length === 0) {
-            return res.status(404).json({ message: "No schemes found" });
-        }
-        res.json(schemes);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// Get scheme by id
-router.get("/scheme/:id", async (req, res) => {
-    try {
-        const scheme = await Scheme.findById(req.params.id);
+        
+        const scheme= await SchemeModel.find();
         if (!scheme) {
             return res.status(404).json({ message: "Scheme not found" });
         }
@@ -43,28 +21,108 @@ router.get("/scheme/:id", async (req, res) => {
     }
 });
 
-// Create a scheme
-router.post("/scheme", async (req, res) => {
+
+// Get scheme by id
+router.get("/:id", async (req, res) => {
     try {
-        const { totalMarks, parameters } = req.body;
-        const scheme = new Scheme({
-            totalMarks,
-            parameters,
-        });
-        await scheme.save();
-        res.status(201).json({ message: "Scheme created successfully", data: scheme });
+        const scheme = await SchemeModel.findById(req.params.id);
+        if (!scheme) {
+            return res.status(404).json({ message: "Scheme not found" });
+        }
+        res.json(scheme);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
 
-// Update a scheme by id
-router.put("/scheme/:id", async (req, res) => {
+//Create a scheme:POST
+router.post("/", async (req, res) => {
+    const schemeSchema = joi.object({
+        courseId:joi.string().required(),
+        totalMarks: joi.number().required(),
+        parameters: joi.array()
+            .items(
+                joi.object({
+                    name: joi.string().required(),
+                    weightage: joi.number().required().min(0).max(100),
+                }),
+            )
+            .required(),
+    });
+    //validate request body against schemeSchema
+    try { const { value: data, error } = schemeSchema.validate(req.body);
+    
+            if (error) {
+                return res.status(400).json({ message: error.details[0].message, success: false });
+            }
+            if (!mongoose.Types.ObjectId.isValid(data.courseId)) {
+                return res.status(400).json({
+                  message: "Invalid Course id",
+                  success: false,
+                });
+              }
+    
+        const course=await CourseModel.findById(data.courseId);
+        if (!course) {
+            return res.status(404).json({
+              message: "Course not found",
+              success: false,
+            });
+          }
+        const scheme = new SchemeModel({
+            courseId:data.courseId,
+           totalMarks:data.totalMarks,
+            parameters:data.parameters,
+
+        });
+        await scheme.save();
+        
+        
+
+        res.status(201).json({ message: "Scheme of the course created successfully", data: scheme });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+
+
+//Update a scheme by id
+router.put("/:id", async (req, res) => {
+    const schemeSchema = joi.object({
+        courseId:joi.string().required(),
+        totalMarks: joi.number().required(),
+        parameters: joi.array()
+            .items(
+                joi.object({
+                    name: joi.string().required(),
+                    weightage: joi.number().required().min(0).max(100),
+                }),
+            )
+            .required(),
+    });
     try {
-        const { totalMarks, parameters } = req.body;
-        const scheme = await Scheme.findByIdAndUpdate(
+        const { value: data, error } = schemeSchema.validate(req.body);
+    
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message, success: false });
+        }
+
+        if (!mongoose.Types.ObjectId.isValid(data.courseId)) {
+          return res.status(400).json({
+            message: "Invalid Course id",
+            success: false,
+          });
+        }
+        
+
+
+
+        const { courseId,totalMarks, parameters } = req.body;
+        const scheme = await SchemeModel.findByIdAndUpdate(
             req.params.id,
-            { totalMarks, parameters },
+            { courseId,totalMarks, parameters },
             { new: true },
         );
         if (!scheme) {
@@ -77,13 +135,14 @@ router.put("/scheme/:id", async (req, res) => {
 });
 
 // Delete a scheme by id
-router.delete("/scheme/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
+    
     try {
-        const scheme = await Scheme.findByIdAndDelete(req.params.id);
+         const scheme = await SchemeModel.findByIdAndDelete(req.params.id);
         if (!scheme) {
             return res.status(404).json({ message: "Scheme not found" });
         }
-        res.json({ message: "Scheme deleted successfully" });
+         res.json({ message: "Scheme deleted successfully" });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
