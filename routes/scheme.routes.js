24f4aee04ef4a8c8071c8a435927f8ebf76f81e2
Abdul Majid <majid616365@gmail.com>
2024-04-03 12:46:24 +0530

    update program

--- routes/scheme.routes.js
+++ routes/scheme.routes.js
@@ -2,12 +2,12 @@ import express from "express";
 
 import schemeController from "../controllers/scheme.controller.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.get("/",schemeController.getSchemeByCourse);
+router.get("/", schemeController.getSchemeByCourse);
 router.get("/:id", schemeController.getSchemeById);
-router.post("/",schemeController.createScheme);
-router.put("/:id",schemeController.updateScheme);
-router.delete("/:id",schemeController.deleteScheme);
- 
+router.post("/", schemeController.createScheme);
+router.put("/:id", schemeController.updateScheme);
+router.delete("/:id", schemeController.deleteScheme);
+
 export default router;
