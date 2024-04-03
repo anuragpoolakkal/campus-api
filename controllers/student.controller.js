eace814cd03fe78fa267c28d42be6a48ad0e663d
Abdul Majid <majid616365@gmail.com>
2024-04-03 12:55:55 +0530

    fix student controlelr

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -1,6 +1,7 @@
 import joi from "joi";
 import studentService from "../services/student.service.js";
 import { handleError } from "../utils/utils.js";
+import logger from "../utils/logger.js";
 
 const createStudent = async (req, res) => {
     try {
@@ -12,20 +13,25 @@ const createStudent = async (req, res) => {
             address: joi.string(),
             rollNo: joi.string().required(),
             collegeId: joi.string().required(),
-            userId: joi.string().required(),
         });
 
         // Validate request body
-        const data = await schema.validateAsync(req.body);
+        const { value: data, error } = schema.validate(req.body);
+
+        if (error) {
+            throw { status: 400, message: error.details[0].message };
+        }
 
         const student = await studentService.create(data, req.user._id, req.user.college._id);
 
+        logger.info("Student registered successfully");
         res.status(201).json({
             message: "Student registered successfully",
             data: student,
             success: true,
         });
     } catch (error) {
+        logger.error(error);
         handleError(res, error);
     }
 };
@@ -33,21 +39,23 @@ const createStudent = async (req, res) => {
 const updateStudent = async (req, res) => {
     try {
         const schema = joi.object({
-            name: joi.string().required(),
-            admNo: joi.string().required(),
-            email: joi.string().email().required(),
-            phone: joi.string().required(),
+            name: joi.string(),
+            admNo: joi.string(),
+            email: joi.string().email(),
+            phone: joi.string(),
             address: joi.string(),
-            rollNo: joi.string().required(),
-            collegeId: joi.string().required(),
+            rollNo: joi.string(),
+            collegeId: joi.string(),
         });
         const { value: data, error } = schema.validate(req.body);
 
         if (error) {
-            return res.status(400).json({ message: error.details[0].message, success: false });
+            throw { status: 400, message: error.details[0].message };
         }
 
         const student = await studentService.update(req.params.id, data);
+
+        logger.info("Student updated successfully");
 
         return res.status(200).json({
             message: "Student updated successfully",
@@ -55,6 +63,7 @@ const updateStudent = async (req, res) => {
             success: true,
         });
     } catch (error) {
+        logger.error(error);
         handleError(res, error);
     }
 };
@@ -63,8 +72,10 @@ const getStudent = async (req, res) => {
     try {
         const student = await studentService.findById(req.params.id);
 
+        logger.info("Student fetched successfully");
         return res.status(200).json({ data: student, success: true });
     } catch (error) {
+        logger.error(error);
         handleError(res, error);
     }
 };
@@ -73,8 +84,10 @@ const getAllStudents = async (req, res) => {
     try {
         const students = await studentService.findAll();
 
+        logger.info("Students fetched successfully");
         return res.status(200).json({ data: students, success: true });
     } catch (error) {
+        logger.error(error);
         handleError(res, error);
     }
 };
@@ -82,9 +95,10 @@ const getAllStudents = async (req, res) => {
 const deleteStudent = async (req, res) => {
     try {
         await studentService.deleteById(req.params.id);
-
+        logger.info("Student deleted successfully");
         return res.status(200).json({ message: "Student deleted successfully", success: true });
     } catch (error) {
+        logger.error(error);
         handleError(res, error);
     }
 };
