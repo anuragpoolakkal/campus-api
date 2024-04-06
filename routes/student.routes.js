fa1a341555de4faa973dc20fdafddf4424cc3d2e
Sre-n <tcr21cs062@gmail.com>
2024-04-06 17:20:49 +0530

    student api updated

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import studentController from "../controllers/student.controller.js";
+import { validateAdmin, validateUser } from "../middlewares/userValidation.js";
 
 const router = express.Router();
 
-router.post("/", studentController.createStudent);
+router.post("/", validateAdmin, studentController.createStudent);
 router.put("/:id", studentController.updateStudent);
-router.get("/:id", studentController.getStudent);
-router.get("/", studentController.getAllStudents);
+router.get("/:id", studentController.getStudentById);
+router.get("/", studentController.getStudents);
 router.delete("/:id", studentController.deleteStudent);
 
 export default router;
