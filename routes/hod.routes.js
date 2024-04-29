4a726d9810f2dab30bc6059ce11f81370f96ff04
Aqeel <aqeelten@gmail.com>
2024-04-29 15:55:37 +0530

    hod route middleware update

--- routes/hod.routes.js
+++ routes/hod.routes.js
@@ -3,7 +3,7 @@ import { validateAdmin, validateUser } from "../middlewares/userValidation.js";
 import hodController from "../controllers/hod.controller.js";
 const router = express.Router();
 
-router.get("/", validateAdmin, hodController.getHods);
+router.get("/", validateUser, hodController.getHods);
 router.get("/:id", validateUser, hodController.getHodById);
 router.post("/", validateUser, hodController.createHod);
 router.put("/:id", validateUser, hodController.updateHod);
